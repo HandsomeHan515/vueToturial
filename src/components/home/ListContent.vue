fe1343e62c4e1e6928a2fbab17700f47c3d75959
hanshuai <hanshuai@dongwu-inc.com>
2018-01-23 20:39:54 +0800

    update home list

--- src/components/home/ListContent.vue
+++ src/components/home/ListContent.vue
@@ -13,32 +13,60 @@
           <span class="show-content-title">{{title}}描述：</span>
           <input class="show-content-input" type="text" :placeholder="`请输入${title}标题`">
         </p>
-        <div class="list-content-button">+ 添加单元（单元包含视频、文件、网页、练习、涂鸦</div>
+        <div class="list-content-button" @click="handleClickShowDialog">+ 添加单元（单元包含视频、文件、网页、练习、涂鸦</div>
         <div class="show-bottom-button">
           <Button type="primary" size="small">保存</Button>
           <Button type="warning" size="small">取消</Button>
         </div>
+        <p class="list-content-hr"></p>
     </div>
+    <Dialog width="80%" :model="false" :show-close="false" :modal-append-to-body="false"  :visible.sync="isDialog" :before-close="handleClickCloseDialog">
+      <Tabs type="card">
+        <TabPane label="新建添加">
+          <div class="dialog-tab-pane-content">1234</div>
+        </TabPane>
+        <TabPane label="选取添加">
+          <div class="dialog-tab-pane-content">5678</div>
+        </TabPane>
+      </Tabs>
+      <div class="show-bottom-button">
+        <Button type="primary" size="small">保存</Button>
+        <Button type="warning" size="small" @click="handleClickCancelDialog">取消</Button>
+      </div>
+    </Dialog>
   </div>
 </template>
 
 <script>
-import { Button } from "element-ui";
+import { Button, Dialog, Tabs, TabPane } from "element-ui";
 export default {
   name: "ListContent",
   props: ["message"],
   data() {
     return {
       isDisplay: true,
+      isDialog: false,
       title: this.message.name.split("", 2).join("")
     };
   },
   components: {
-    Button
+    Button,
+    Dialog,
+    Tabs,
+    TabPane
   },
   methods: {
     handleClickShow() {
       this.isDisplay = !this.isDisplay;
+    },
+    handleClickShowDialog() {
+      this.isDialog = true;
+    },
+    handleClickCloseDialog(done) {
+      done();
+    },
+    handleClickCancelDialog() {
+      this.isDialog = false;
     }
   }
 };
@@ -71,10 +99,12 @@ export default {
 .show-content-title {
   font-size: 14px;
   color: #333;
+  width: 15%;
 }
 .show-content-input {
   border: none;
   outline: 0;
+  width: 85%;
 }
 .show-content-input::-webkit-input-placeholder {
   color: #ccc;
@@ -94,11 +124,18 @@ export default {
   text-align: center;
   border: 1px dashed #ccc;
   font-size: 14px;
+  text-shadow: none;
 }
 .show-bottom-button {
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
   margin: 10px 10px 0 0;
+}
+.list-content-hr {
+  border-bottom: 1px solid #f2f2f2;
+}
+.dialog-tab-pane-content {
+  height: 300px;
 }
 </style>
